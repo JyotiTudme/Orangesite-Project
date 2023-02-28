c42079f6cdd3c6581918e438be02d1a8f5920fe7
JyotiTudme <122000336+JyotiTudme@users.noreply.github.com>
2023-02-28 17:04:54 +0530

    Add files via upload

---
+++ Logout.java
@@ -0,0 +1,37 @@
+package project;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Logout {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		Properties prop = new Properties();
+		FileInputStream ip= new FileInputStream("C:\\Users\\shubh\\eclipse-workspace\\Exclerselenium\\src\\test\\java\\project\\logout.properties");
+	       prop.load(ip);
+	       driver.get(prop.getProperty("url"));
+	       driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+	       driver.findElement(By.xpath(prop.getProperty("chrome.login.username.xpath"))).
+	       sendKeys(prop.getProperty("name"));
+	       driver.findElement(By.xpath(prop.getProperty("chrome.login.password.xpath"))).
+	       sendKeys(prop.getProperty("pass"));
+	       driver.findElement(By.xpath(prop.getProperty("chrome.login.login.xpath"))).click();
+	    driver.findElement(By.xpath(prop.getProperty("chrome.paul.xpath"))).click();
+	    Thread.sleep(2000);
+	      driver.findElement(By.xpath(prop.getProperty("chrome.logout.xpath"))).click();
+	      
+	}
+}
